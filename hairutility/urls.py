0984de353d3c3dee068c47bcbdd96d8941f32467
jtruo <jazure622@gmail.com>
2018-07-19 10:57:36 -0400

    Fixed flake

--- hairutility/urls.py
+++ hairutility/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.urls import path, re_path, include
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib import admin
 
