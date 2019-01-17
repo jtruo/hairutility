b65a307d7bf465484d1423bb112ad331cf493c10
jtruo <jazure622@gmail.com>
2019-01-17 18:57:56 -0500

    Fixed env variables for local/docker-compose purposes

--- hairutility/webapp/views.py
+++ hairutility/webapp/views.py
@@ -4,9 +4,9 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import boto3
 
 
-# s3 = boto3.resource('s3')
-# client = boto3.client('s3')
-# my_bucket = s3.Bucket('hairutility-prod')
+s3 = boto3.resource('s3')
+client = boto3.client('s3')
+my_bucket = s3.Bucket('hairutility-prod')
 
 
 class HomePageView(TemplateView):
@@ -19,19 +19,19 @@ class AboutUsPageView(TemplateView):
 
 def hair_profiles(request):
 
-    # key_list = []
+    key_list = []
 
-    # for key in my_bucket.objects.filter(Prefix='images/'):
-    #     key_urls = 'https://s3.us-east-2.amazonaws.com/hairutility-prod/' + key.key
-    #     print(key_urls)
-    #     key_list.append(key_urls)
+    for key in my_bucket.objects.filter(Prefix='images/'):
+        key_urls = 'https://s3.us-east-2.amazonaws.com/hairutility-prod/' + key.key
+        print(key_urls)
+        key_list.append(key_urls)
 
-    # paginator = Paginator(key_list, 5)
+    paginator = Paginator(key_list, 5)
 
-    # page = request.GET.get('page')
+    page = request.GET.get('page')
 
-    # keys = paginator.get_page(page)
+    keys = paginator.get_page(page)
 
-    return render(request, 'hair-profiles.html')
+    # return render(request, 'hair-profiles.html')
 
-    # return render(request, 'hair-profiles.html', {'keys': keys})
+    return render(request, 'hair-profiles.html', {'keys': keys})
